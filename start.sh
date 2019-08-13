09eb089819bb0957b45d546f3727b120d5fa5043
vincentouma <vinceoumah@gmail.com>
2019-08-13 07:40:03 +0300

    Deployment

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@ export SECRET_KEY="vince"
 export  MAIL_USERNAME="vinceobindi@gmail.com"
 export MAIL_PASSWORD="0717654230"
 export DATABASE_URL = 'postgresql+psycopg2://moringa :vinceobindi1005@localhost/post'
-export
+
 python3.6 manage.py server 
